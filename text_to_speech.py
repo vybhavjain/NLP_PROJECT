a009920b3e5ad9f3543150a1d05c97c5e9f1640d
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-11-11 17:41:35 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov  4 12:13:08 2018
-
-@author: VYBHAV JAIN
-"""
-
 import turtle
 import math
 
@@ -35,46 +28,123 @@ def circle(radius,color,color1):
     turtle.end_fill()
     return  
 
+def normalcircle(radius,color):   
+    x = color
+    #y = color1
+    turtle.up()
+    # go to (0, radius)
+    turtle.goto(0,radius)
+    turtle.down()    
+    turtle.color(x)
+    #turtle.begin_fill()
+    # number of times the y axis has been crossed
+    times_crossed_y = 0
+    x_sign = 1.0
+    while times_crossed_y <= 1:
+        # move by 1/360 circumference
+        turtle.forward(2*math.pi*radius/360.0)
+        # rotate by one degree (there will be
+        # approx. 360 such rotations)
+        turtle.right(1.0)
+        # we use the copysign function to get the sign
+        # of turtle's x coordinate
+        x_sign_new = math.copysign(1, turtle.xcor())        
+        if(x_sign_new != x_sign):
+            times_crossed_y += 1
+        x_sign = x_sign_new
+    #turtle.end_fill()
+    return  
+
 
 
 def square1(dim,color,color1):
     x = color
-    y = color1    
-    turtle.color(x,y)
+    z = color1 # this refers to the color that is filled in the middle
+    turtle.color(x,z)
     turtle.begin_fill()
-    turtle.forward(dim)
-    turtle.left(90)
-    turtle.forward(dim)
-    turtle.left(90)
-    turtle.forward(dim)
-    turtle.left(90)
-    turtle.forward(dim)
-    turtle.left(90)
+    for i in range (4):
+        turtle.forward(dim)
+        turtle.left(90)
+   # turtle.forward(dim)
+   # turtle.left(90)
+   # turtle.forward(dim)
+   # turtle.left(90)
+   # turtle.forward(dim)
+   # turtle.left(90)
     turtle.end_fill()
     return
 
-def rectangle1(dim1,dim2,color,color1): #color 1 is going to be filled inside
+def rectangle1(dim1,dim2,color,color1):
     x = color
-    y = color1
-    turtle.color(x,y)
+    z = color1 # this refers to the color that is filled in the middle
+    turtle.color(x,z)
     turtle.begin_fill()
-    turtle.forward(dim1)
-    turtle.left(90)
-    turtle.forward(dim2)
-    turtle.left(90)
-    turtle.forward(dim1)
-    turtle.left(90)
-    turtle.forward(dim2)
-    turtle.left(90)
+    for i in range (2):
+        turtle.forward(dim1)
+        turtle.left(90)
+        turtle.forward(dim2)
+        turtle.left(90)
+    #turtle.forward(dim1)
+    #turtle.left(90)
+    #turtle.forward(dim2)
+    #turtle.left(90)
     turtle.end_fill()
+    return    
+    
+def star(color,color1):
+    x = color
+    z = color1 # this refers to the color that is filled in the middle
+    turtle.color(x,z)
+    #color(x,z)
+    turtle.begin_fill()
+    while True:
+        turtle.forward(200)
+        turtle.left(170)
+        if abs(turtle.pos()) < 1:
+            break
+    turtle.end_fill()
+    turtle.done()
 
-# for circle ,pass radius ,pen color and then filling color
-# for square,pass side,pen color and then filling color
-# for rectangle ,pass breadth,height ,pen color and then filling color
+def triangle(color,color1): # equilateral
+    x = color
+    z = color1 # this refers to the color that is filled in the middle
+    turtle.color(x,z)
+    #color(x,z)
+    turtle.begin_fill()
+    while True:
+        turtle.forward(200)
+        turtle.left(120)
+        if abs(turtle.pos()) < 1:
+            break
+    turtle.end_fill()
+    turtle.done()
+    
+def starcircle(color,color1):
+    x = color
+    z = color1 # this refers to the color that is filled in the middle
+    turtle.color(x,z)
+    #color(x,z)
+    turtle.begin_fill()
+    while True:
+        turtle.forward(200)
+        turtle.left(170)
+        if abs(turtle.pos()) < 1:
+            break
+    turtle.end_fill()
+    turtle.left(90)
+    turtle.forward(100)
+    normalcircle(100,y)
+    turtle.done()
+
+
 y = 'blue'    # color to be passed 
-# circle (100,'orange','red')    ## verified that its working
-#rectangle1(100,10,'orange','red')  ## verified that its working    
-#square1(50,y)
+a = 'red'
+#triangle(y,a)   
+#square1 (100,a,y)
+#rectangle1(100,30,a,y)    
+#circle(50,y,a)
+starcircle(y,a)
 print('finished')
 turtle.done()
+
 
