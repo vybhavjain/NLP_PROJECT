0a90897cd64e9a9b74fbb73261de6aab5e2b6ebb
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-11-12 22:02:08 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -28,55 +28,6 @@ def circle(radius,color,color1):
     turtle.end_fill()
     return  
 
-'''
-def normalcircle(radius,color):   
-    x = color
-    #y = color1
-    ##turtle.up()
-    # go to (0, radius)
-    ##turtle.goto(0,radius)
-    ##turtle.down()    
-    turtle.color(x)
-    #turtle.begin_fill()
-    # number of times the y axis has been crossed
-    times_crossed_y = 0
-    x_sign = 1.0
-    while times_crossed_y <= 1:
-        # move by 1/360 circumference
-        turtle.forward(2*math.pi*radius/360.0)
-        # rotate by one degree (there will be
-        # approx. 360 such rotations)
-        turtle.right(1.0)
-        # we use the copysign function to get the sign
-        # of turtle's x coordinate
-        x_sign_new = math.copysign(1, turtle.xcor())        
-        if(x_sign_new != x_sign):
-            times_crossed_y += 1
-        x_sign = x_sign_new
-    #turtle.end_fill()
-    return  
-'''
-
-
-'''
-def square1(dim,color,color1):
-    x = color
-    z = color1 # this refers to the color that is filled in the middle
-    turtle.color(x,z)
-    turtle.begin_fill()
-    for i in range (4):
-        turtle.forward(dim)
-        turtle.left(90)
-   # turtle.forward(dim)
-   # turtle.left(90)
-   # turtle.forward(dim)
-   # turtle.left(90)
-   # turtle.forward(dim)
-   # turtle.left(90)
-    turtle.end_fill()
-    return
-'''
-
 def rectangle1(dim1,dim2,color,color1):
     x = color
     z = color1 # this refers to the color that is filled in the middle
@@ -112,24 +63,6 @@ def design():   # needs to be worked on and checked
         turtle.left(123)
     
     turtle.done()
-    
-
-'''
-def triangle(color,color1): # equilateral
-    x = color
-    z = color1 # this refers to the color that is filled in the middle
-    turtle.color(x,z)
-    #color(x,z)
-    turtle.begin_fill()
-    while True:
-        turtle.forward(200)
-        turtle.left(120)
-        if abs(turtle.pos()) < 1:
-            break
-    turtle.end_fill()
-    turtle.done()
-'''
-
     
 def polygon(n,length,color,color1):
     x = color
@@ -177,5 +110,3 @@ a = 'red'
 starcircle(y,a)
 print('finished')
 turtle.done()
-
-
