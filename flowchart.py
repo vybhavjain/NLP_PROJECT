2524c9f4c0a0123bf1fc6d98ef83fc2ff83b841d
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-11-15 16:05:33 +0530

    Update flowchart.py

--- flowchart.py
+++ flowchart.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 13 21:55:12 2018
+Created on Thu Nov 15 14:33:41 2018
 
 @author: VYBHAV JAIN
 """
@@ -17,5 +17,42 @@ def flatoval(r):               # Horizontal Oval
         turtle.circle(r,90)
         turtle.circle(r/2,90)
         
-flatoval(20)
-turtle.write("anything")
+def rectangle():
+    turtle.down()
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.left(90)
+    #turtle.right(20)
+    #turtle.forward(20)
+
+turtle.setpos(0,60)
+flatoval(25)
+turtle.write("Start")
+
+turtle.left(45)
+turtle.setpos(0,0)
+#turtle.setpos(-60,0)
+rectangle()
+turtle.write("1.Receive 2 numbers from the user")
+turtle.setpos(0,-20)
+
+
+turtle.setpos(0,-60)
+rectangle()
+turtle.write("2.Add both the numbers")
+
+turtle.setpos(0,-120)
+rectangle()
+turtle.write("3.Display the result")
+
+turtle.setpos(0,-180)
+flatoval(25)
+turtle.write("Stop")
+
+
+
