e7eff6d01ee736bf84f19b0c94b0e79267ba853f
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-11-24 17:51:57 +0530

    Update flow_test_incomplete.py

--- flow_test_incomplete.py
+++ flow_test_incomplete.py
@@ -12,9 +12,9 @@ def flatoval(r):               # Horizontal Oval
         turtle.circle(r/2,90)
         
 def start():
-    #turtle.penup()   # makes pen disappear in the current block only
+    turtle.penup()   # makes pen disappear in the current block only
     turtle.setpos(0,60)
-    #turtle.pendown()
+    turtle.pendown()
     turtle.write("Start")
     flatoval(25)
     #turtle.write("Start")
@@ -22,35 +22,57 @@ def start():
     #turtle.circle(25/2,90)
 
 def stop():
+   turtle.setpos(0,-167.5)
+   turtle.penup()   # makes pen disappear in the current block only
    turtle.setpos(0,-180)
+   turtle.pendown()   
    flatoval(25)
    turtle.write("Stop")
 
-def rectangle(): 
-   turtle.down()    
-   for i in range(2):
-        turtle.forward(200)
+def rectangle(x,y): 
+   #turtle.down()    
+   #for i in range(2):
+        turtle.forward(y)
         turtle.left(90)
         turtle.forward(20)
         turtle.left(90)
+        turtle.forward(2*y)
+        turtle.left(90)
+        turtle.forward(20)
+        turtle.left(90)
+        turtle.write(x)
+        turtle.forward(y)
     
 def flowchart(x):
 
     start()   # function to print the start inside the oval
 
+    import turtle
     turtle.left(45)
+    turtle.setpos(0,20)
+    turtle.penup()   # makes pen disappear in the current block only
     turtle.setpos(0,0)
+    turtle.pendown()
     #turtle.setpos(-60,0)
-    rectangle()
-    turtle.write("1.Receive 2 numbers from the user")
-    turtle.setpos(0,-20)
+    rectangle(" 1.Receive 2 numbers from the user",90)
+    #turtle.write("1.Receive 2 numbers from the user")
+    #turtle.setpos(0,-20)
+    turtle.setpos(0,-40)
+    turtle.penup()   # makes pen disappear in the current block only
     turtle.setpos(0,-60)
-    rectangle()
-    y = "2." + x + " both the numbers" # need to check if this working 
-    turtle.write(y)
+    turtle.pendown()
+    #turtle.setpos(0,-60)
+    #rectangle()
+    y = " 2." + x + " both the numbers" # need to check if this working 
+    #turtle.write(y)
+    rectangle(y,60)
+    turtle.setpos(0,-100)
+    turtle.penup()   # makes pen disappear in the current block only
     turtle.setpos(0,-120)
-    rectangle()
-    turtle.write("3.Display the result")
+    turtle.pendown()
+    #turtle.setpos(0,-120)
+    rectangle(" 3.Display the result",50)
+    #turtle.write("3.Display the result")
 
     stop()  # function to print inside oval stop
     turtle.done()
