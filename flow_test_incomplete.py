1fb8b5b482b6f8a5c2c676d5d1be76ca1ea339be
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-11-24 16:33:37 +0530

    Create flow_test_incomplete.py

---
+++ flow_test_incomplete.py
@@ -0,0 +1,62 @@
+# try if condition , rhombus for an example
+# make neater
+# try arranging in centre
+
+import turtle 
+
+def flatoval(r):               # Horizontal Oval
+    turtle.right(45)
+    #turtle.write("Start")
+    for loop in range(2):
+        turtle.circle(r,90)
+        turtle.circle(r/2,90)
+        
+def start():
+    #turtle.penup()   # makes pen disappear in the current block only
+    turtle.setpos(0,60)
+    #turtle.pendown()
+    turtle.write("Start")
+    flatoval(25)
+    #turtle.write("Start")
+    #turtle.circle(25,90)
+    #turtle.circle(25/2,90)
+
+def stop():
+   turtle.setpos(0,-180)
+   flatoval(25)
+   turtle.write("Stop")
+
+def rectangle(): 
+   turtle.down()    
+   for i in range(2):
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(20)
+        turtle.left(90)
+    
+def flowchart(x):
+
+    start()   # function to print the start inside the oval
+
+    turtle.left(45)
+    turtle.setpos(0,0)
+    #turtle.setpos(-60,0)
+    rectangle()
+    turtle.write("1.Receive 2 numbers from the user")
+    turtle.setpos(0,-20)
+    turtle.setpos(0,-60)
+    rectangle()
+    y = "2." + x + " both the numbers" # need to check if this working 
+    turtle.write(y)
+    turtle.setpos(0,-120)
+    rectangle()
+    turtle.write("3.Display the result")
+
+    stop()  # function to print inside oval stop
+    turtle.done()
+
+print('enter one of the following operations to perform on two numbers')
+#print(“ Add , subtract ,multiply , divide”)
+#x = input('Add, subtract ,multiply,divide')
+flowchart('add')
+
